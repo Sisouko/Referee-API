ca8770cb28e2e0c6c31b02a8fbb4bcfe10983ca6
Sisouko <ahmedabdrebbi48@gmail.com>
2026-07-03 17:20:19 +0200

    Controller-File

--- controllers/match.controller.js
+++ controllers/match.controller.js
@@ -21,3 +21,46 @@ exports.getMatchById = async (req, res, next) => {
         next (err)
     }
 }
+
+exports.createMatch = async (req,  res, next) => {
+    try {
+        const newMatch = await Match.create(req.body)
+        res.status(201).json(newMatch)
+    } catch (err) {
+        next (err)
+    }
+}
+
+exports.updateMatch = async (req, res, next) => {
+    try{
+        const {id} = req.params
+        const match = await Match.findByPk(id)
+        if(!match) {
+            return res.status(404).json({ error: 'Match not found'})
+        }
+        await match.update(req.body)
+        res.status(200).json(match)
+    } catch (err) {
+        next (err)
+    }
+}
+
+exports.deleteMatch = async (req, res, next) => {
+    try{
+        const {id} = req.params
+        const match = await Match.findByPk(id)
+        if(!match) {
+            return res.status(404).({ error: 'Match not found' })
+        }
+        await match.destroy()
+        res.status(204).send
+    } catch (err) {
+        next (err)
+    }
+}
+
+exports.getMatchArbitres = async (req, res, next) => {
+    try {
+        const {id}
+    }
+}
