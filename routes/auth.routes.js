5ae2e35f78d95a5fde93eaed716cb1c625727bdb
Sisouko <ahmedabdrebbi48@gmail.com>
2026-07-09 14:12:21 +0200

    add--auth.route--FILE

---
+++ routes/auth.routes.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const router = express.Router()
+
+    const authController = require('../controllers/auth.controller')
+    const authenticate = require('../middlewares/authenticate.middleware')
+    const authorize = require('../middlewares/authorize.middleware')
+
+    router.post('/register', authenticate, authorize('admin'), authController.register)
+    router.post('/login', authController.login)
+    router.get('/me', authenticate, authController.me)
+
+    module.exports= router
